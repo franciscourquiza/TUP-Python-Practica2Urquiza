90d8f8b5571f93fc541cfab6c2efaba73b45369e
Francisco Urquiza <103083880+franciscourquiza@users.noreply.github.com>
2022-09-14 01:12:44 -0300

    Add files via upload

---
+++ exercises/P2Ejercicio4.py
@@ -0,0 +1,84 @@
+"""Expresiones Booleanas."""
+
+
+from tkinter.messagebox import RETRY
+from unittest import case
+
+
+def es_vocal_if(letra: str) -> bool:
+    if letra == "a":
+        return True
+    if letra == "b":
+        return False
+    if letra == "A":
+        return True
+    if letra == "e":
+        return True
+    if letra == "E":
+        return True
+    return False
+    """Toma un string y devuelve un booleano en base a si letra es una vocal o
+    no.
+    Restricciónes:
+        - Utilizar un if para cada posibilidad.
+        - Utilizar la función lower() sólo una vez.
+        - No utilizar ELSE.
+        - Utilizar 6 returns.
+    Referencia: https://docs.python.org/3/library/stdtypes.html#string-methods
+    """
+
+
+# NO MODIFICAR - INICIO
+assert es_vocal_if("a")
+assert not es_vocal_if("b")
+assert es_vocal_if("A")
+assert es_vocal_if("e")
+assert es_vocal_if("E")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_if_in(letra: str) -> bool:
+    if letra in "a" or letra in "A" or letra in "e" or letra in "E":
+        return True
+    return False
+    """Re-escribir utilizando un sólo IF y el operador IN.
+    Restricciónes:
+        - Utilizar un único IF.
+        - Utilizar dos returns.
+        - No utilizar ELSE.
+        - No utilizar FOR.
+        - No utilizar listas.
+    Referencia: https://docs.python.org/3/reference/expressions.html#membership-test-operations # noqa: E501
+    """
+
+
+# NO MODIFICAR - INICIO
+assert es_vocal_if_in("a")
+assert not es_vocal_if_in("b")
+assert es_vocal_if_in("A")
+# NO MODIFICAR - FIN
+
+
+###############################################################################
+
+
+def es_vocal_in(letra: str) -> bool:
+    while letra in "a"or letra in "A" or letra in "e" or letra in "E":
+        return True
+    """Re-escribir como expresión booleana utilizando el operador IN
+    Restricciónes:
+        - No utilizar IF.
+        - Utilizar un único return.
+        - No utilizar FOR.
+        - No utilizar listas.
+    """
+
+
+# NO MODIFICAR - INICIO
+assert es_vocal_in("a")
+assert not es_vocal_in("b")
+assert es_vocal_in("A")
+# NO MODIFICAR - FIN
